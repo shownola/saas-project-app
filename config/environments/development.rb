1312b94af8fe9ef3e913fd0a708bea1a292b55e8
SHERRY <swasieko@gmail.com>
2016-12-22 19:38:43 +0000

    Add email with Sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,10 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'https://completerubydevcoursepart2-shownola.c9users.io' }
+
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
